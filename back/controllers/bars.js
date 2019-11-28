116935de70fc16b2bcc586ccc257460a113d2af5
Katri Rasio <katri.rasio@metropolia.fi>
2019-11-28 10:32:02 +0200

    bars & bar

---
+++ back/controllers/bars.js
@@ -0,0 +1,50 @@
+const barsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    console.log('täälööä');
+    return authorization.substring(7);
+  }
+  return null;
+};
+
+barsRouter.get('/', async (request, response) => {
+  const bars = await Bar
+  .find({}).populate('user', { username: 1, name: 1 });
+  console.log(bars);
+  response.json(bars.map(bar => bar.toJSON()));
+});
+
+barsRouter.post('/', async (request, response, next) => {
+  const body = request.body;
+
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!request.token || !decodedToken.id) {
+      console.log('back/controller/bars/65');
+      return response.status(401).json({ error: 'token missing or invalid' });
+    }
+
+    const user = await User.findById(decodedToken.id);
+
+    const bar = new Bar({
+      name: body.name,
+      address: body.address,
+      city: body.city,
+      prices: body.prices,
+      likes: body.likes === undefined ? 0 : body.likes,
+      user: user._id
+    });
+
+    const savedBar = await bar.save();
+    user.bars = user.cars.concat(savedBar._id);
+    await user.save();
+    response.json(savedBar.toJSON());
+  } catch (exception) {
+    next(exception);
+  }
+});
---
+++ back/models/bar.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+//const uniqueValidator = require('mongoose-unique-validator');
+
+const barSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  prices: {
+    beer: Number,
+    cider: Number,
+    longdrink: Number,
+  },
+  likes: Number
+});
+
+barSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Bar', barSchema);
