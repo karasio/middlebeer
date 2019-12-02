599e4a9c0ec8c7c03664233faf6172cdac5d8182
Jesse Väärälä <jesse.vaarala@gmail.com>
2019-12-02 10:01:52 +0200

    baarinäkymän filtteröinti

--- front/src/components/FrontPage.js
+++ front/src/components/FrontPage.js
@@ -1,46 +1,60 @@
 import React, {useEffect, useState} from 'react';
 import Bar from '../components/Bar'
 import barService from '../services/bars';
-import { useField } from '../hooks';
+import {useField} from '../hooks';
 import '../components/FrontPage.css'
 
 const FrontPage = ({user}) => {
-  const [bars, setBars] = useState([]);
-  const filterValue = useField('type: text');
+    const [bars, setBars] = useState([]);
+    const filterValue = useField('type: text');
 
-  useEffect( () => {
-    barService
-    .getAll()
-    .then(initialBars => setBars(initialBars));
-  }, []);
+    useEffect(() => {
+        barService
+            .getAll()
+            .then(initialBars => setBars(initialBars));
+    }, []);
 
-  useEffect(() => {
-    console.log('FrontPage useEffect',bars);
-  }, [bars]);
+    useEffect(() => {
+        console.log('FrontPage useEffect', bars);
+    }, [bars]);
 
-  const allBars = () =>
-    bars.map(bar => {
-      return (
-          <Bar
-              key={bar.id}
-              bar={bar}
-          />
-      )
-    });
+    const barsToShow = (barArray) =>
+        barArray.map(bar => {
+            return (
+                <Bar
+                    key={bar.id}
+                    bar={bar}
+                />
+            )
+        });
 
-  return (
-      <div>
-        <form>
-          {/* tämä menee hooks > index.js:lle lomakkeen käsittelyyn, pitäisi
+    const getFilteredBars = (exp) => {
+        let pattern = new RegExp(exp.object.value, 'i')
+        let filteredBarsCopy = []
+
+        bars.forEach(bar => {
+            // console.log('filtering', pattern, bar.name, pattern.test(bar.name))
+            if (pattern.test(bar.name)) {
+                filteredBarsCopy.push(bar)
+            }
+        })
+        return filteredBarsCopy
+    }
+
+    return (
+        <div>
+            <form>
+                {/* tämä menee hooks > index.js:lle lomakkeen käsittelyyn, pitäisi
           passata filtteriarvoksi allBarsille?*/}
-          <input
-              {...filterValue.object}
-              placeholder={'filter results'}
-          />
-        </form>
-        {allBars()}
-      </div>
-  );
+                <input
+                    {...filterValue.object}
+                    placeholder={'filter results'}
+                />
+            </form>
+            {barsToShow(getFilteredBars(filterValue))}
+        </div>
+    );
 };
+
 
 export default FrontPage;
