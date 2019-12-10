1bd4be5d1164ad8b3180a6b7f37f4364eb1c9990
Samu <samu.rinne@metropolia.com>
2019-12-10 11:58:37 +0200

    Excludes invalid characters from search

--- front/src/hooks/index.js
+++ front/src/hooks/index.js
@@ -10,7 +10,13 @@ export const useField = (type) => {
     const [value, setValue] = useState('');
 
     const onChange = (e) => {
-        setValue(e.target.value);
+        const illegalChar = ['?', '(', ')', '[', ']', '*', '\\'];
+        if (illegalChar.some(el => e.target.value.includes(el))){
+            //TODO Tänne joku hieno juttu, kun yrittää syöttää virheellisiä merkkejä. virheilmotus tms.
+            console.log("JEEEE");
+        }else {
+            setValue(e.target.value);
+        }
     };
 
     const reset = () => {
