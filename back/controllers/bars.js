42f9b7c0bf76c05874beaf8272d7be527c111db7
Katri Rasio <katri.rasio@metropolia.fi>
2019-12-10 17:37:02 +0200

    jsdoc commenting

--- back/controllers/bars.js
+++ back/controllers/bars.js
@@ -3,20 +3,24 @@ const Bar = require('../models/bar');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-};
+/**
+ * Handles contacts to MongoDB
+ */
 
+/**
+ * GET request for getting all bars from database
+ * response: all bars as json
+ */
 barsRouter.get('/', async (request, response) => {
   const bars = await Bar
   .find({}).populate('user', { username: 1, name: 1 });
   response.json(bars.map(bar => bar.toJSON()));
 });
 
+/**
+ * GET request for getting a specific bar with id from database
+ * response: bar information as json
+ */
 barsRouter.get('/:id', async (request, response) => {
   const barId = request.params.id;
   const bar = await  Bar
@@ -24,9 +28,15 @@ barsRouter.get('/:id', async (request, response) => {
   response.json(bar);
 });
 
+/**
+ * PUT request for changing specific bar data with id value in database
+ * validates user with json web token
+ * Changeable values: likes & prices
+ * response: all bars as json
+ */
 barsRouter.put('/:id', async (request, response, next) => {
   const body = request.body;
-  console.log('TÖÖÖÖÖÖÖÖÖÖÖ', body);
+  console.log(body);
   try {
     const decodedToken = jwt.verify(request.token, process.env.SECRET);
     if (!request.token || !decodedToken.id) {
@@ -48,6 +58,11 @@ barsRouter.put('/:id', async (request, response, next) => {
   }
 });
 
+/**
+ * POST request for adding a bar to database
+ * validates user with json web token
+ * response: all bars as json
+ */
 barsRouter.post('/', async (request, response, next) => {
   const body = request.body;
 
@@ -80,6 +95,12 @@ barsRouter.post('/', async (request, response, next) => {
     next(exception);
   }
 });
+
+/**
+ * DELETE request for deleting a bar with specific id from database
+ * validates user with json web token => bar deleting possible only for person who added it
+ * response: status 204 if succeeded, 403 if not
+ */
 
 barsRouter.delete('/:id', async (request, response, next) => {
   const bar = await Bar.findById(request.params.id);
